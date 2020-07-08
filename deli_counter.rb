ed2a42f5a6d273294de53791dfe749371fa26e7c
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 18:11:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,15 @@ take_a_number(katz_deli, "Logan")
 take_a_number(katz_deli, "Avi")
 take_a_number(katz_deli, "Spencer")
 
+def line(array) 
+  if array.length == 0 
+    puts "The line is currently empty."
+  else 
+    message = "The line is currently:"
+    
+  array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
+end 
