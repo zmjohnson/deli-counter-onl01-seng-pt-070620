b90b5b6c903aa10d175af5478121be871f98a7eb
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 18:17:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,3 +26,15 @@ def line(array)
   end 
 end 
 
+line(katz_deli)
+
+
+def now_serving(array)
+  if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
+
+now_serving(katz_deli)
