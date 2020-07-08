d81e0a689d12c808ad02489b6bf9be8eef60a282
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-08 18:06:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,10 @@
 # Write your code here.
 
+katz_deli = [] 
+
+def take_a_number(array, name)
+  array.push(name)
+  position = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, position 
+end 
